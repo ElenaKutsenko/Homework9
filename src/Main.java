83f10e7971a95d490c6236fa4fd2cd8284866997
Elena Kutsenko <albion.k@mail.ru>
2022-06-07 19:06:17 +0300

    правка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Beeper horn = new Horn();
+        EnginV12 enginV12 = new EnginV12();
+        new Lambo(horn, enginV12);
+
+        {
+            System.out.println(Lambo.beeper.makeSound());
+
+        }
+        {
+            System.out.println(Lambo.engin.getSpeed());
+        }
+
+        BusSeats busseats = new BusSeats();
+        BusWheel buswheel = new BusWheel();
+        new Bus(busseats, buswheel);
+        {
+            System.out.println(Bus.passengerseats.Seats());
+        }
+
+        {
+            System.out.println(Bus.wheel.Drive());
+        }
+
+        MiniBusSeats minibusseats = new MiniBusSeats();
+        MiniBusWheel minibuswheel = new MiniBusWheel();
+        new Minibus(minibusseats, minibuswheel);
+
+        {
+            System.out.println(Minibus.passengerseats.Seats());
+        }
+        {
+            System.out.println(Minibus.wheel.Drive());
+        }
     }
+
+
 }
+
+
+
+
